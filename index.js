ee130faa39dd318aca666a51740360ba229253a0
nthokiesther09 <nthokiesther09@gmail.com>
2022-12-02 16:25:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+    return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+ }
+
+ console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+
+ function countDown(number) {
+     while (number > 0) {
+          console.log(number);
+         number--;
+         }
+     console.log(number);}
